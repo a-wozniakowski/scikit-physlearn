d6963c2ebd8aaa067f4f5711953a03627ecdabaa
a-wozniakowski <wozn0001@e.ntu.edu.sg>
2020-06-29 19:16:51 +0800

    Add long description

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ from setuptools import setup, find_packages
 
 def setup_package():
 
-    # with open('README', 'r') as f:
-    #     long_description = f.read()
+    with open('README', 'r') as f:
+        long_description = f.read()
 
     setup(name='physlearn',
           version='0.1',
           license='MIT',
           description='A multi-target regression suite with a regressor dictionary.',
-          #long_description=long_description,
+          long_description=long_description,
           author='Alex Wozniakowski',
           author_email='wozn0001@e.ntu.edu.sg',
           packages=find_packages())
