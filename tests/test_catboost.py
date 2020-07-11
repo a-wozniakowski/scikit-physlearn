7a914abd0727e87e32d747ae2fa9032e3ea2658d
a-wozniakowski <wozn0001@e.ntu.edu.sg>
2020-07-11 18:59:43 +0800

    Include boosting tests

---
+++ tests/test_catboost.py
@@ -0,0 +1,69 @@
+import unittest
+
+import pandas as pd
+
+from scipy.stats import uniform
+from sklearn import __version__ as sk_version
+from sklearn.datasets import load_boston, load_linnerud
+from sklearn.model_selection import train_test_split
+
+from physlearn import Regressor
+
+
+class TestBasic(unittest.TestCase):
+
+    def test_regressor_fit_score(self):
+        X, y = load_boston(return_X_y=True)
+        X, y = pd.DataFrame(X), pd.Series(y)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+
+        params = dict(iterations=10, loss_function='RMSE')
+        reg = Regressor(regressor_choice='catboostregressor', pipeline_transform='standardscaler',
+                        params=params)
+        reg.fit(X_train, y_train)
+        y_pred = reg.fit(X_train, y_train).predict(X_test)
+        score = reg.score(y_test, y_pred)
+        self.assertCountEqual(y_pred.index, y_test.index)
+        self.assertGreaterEqual(score['mae'].values, 0.0)
+        self.assertGreaterEqual(score['mse'].values, 0.0)
+        self.assertLess(score['mae'].values, 2.7)
+        self.assertLess(score['mse'].values, 17.0)
+
+    # sklearn < 0.23 does not have as_frame parameter
+    @unittest.skipIf(sk_version < '0.23.0', 'scikit-learn version is less than 0.23')
+    def test_multioutput_regressor(self):
+        bunch = load_linnerud(as_frame=True)  # returns a Bunch instance
+        X, y = bunch['data'], bunch['target']
+        X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+
+        params = dict(iterations=10, loss_function='RMSE')
+        reg = Regressor(regressor_choice='catboostregressor', pipeline_transform='standardscaler',
+                        params=params)
+        y_pred = reg.fit(X_train, y_train).predict(X_test)
+        score = reg.score(y_test, y_pred).mean()
+        self.assertCountEqual(y_pred.index, y_test.index)
+        self.assertGreaterEqual(score['mae'], 0.0)
+        self.assertGreaterEqual(score['mse'], 0.0)
+        self.assertLess(score['mae'], 12.0)
+        self.assertLess(score['mse'], 250.0)
+
+    # sklearn < 0.23 does not have as_frame parameter
+    @unittest.skipIf(sk_version < '0.23.0', 'scikit-learn version is less than 0.23')
+    def test_multioutput_regressorchain(self):
+        bunch = load_linnerud(as_frame=True)  # returns a Bunch instance
+        X, y = bunch['data'], bunch['target']
+        X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+
+        params = dict(iterations=10, loss_function='RMSE')
+        reg = Regressor(regressor_choice='catboostregressor', pipeline_transform='standardscaler',
+                        params=params, chain_order=[0, 2, 1])
+        y_pred = reg.fit(X_train, y_train).predict(X_test)
+        score = reg.score(y_test, y_pred).mean()
+        self.assertCountEqual(y_pred.index, y_test.index)
+        self.assertGreaterEqual(score['mae'], 0.0)
+        self.assertGreaterEqual(score['mse'], 0.0)
+        self.assertLess(score['mae'], 11.0)
+        self.assertLess(score['mse'], 240.0)
+
+if __name__ == '__main__':
+    unittest.main()
