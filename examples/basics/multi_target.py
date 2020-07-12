0c439ebf0eee720da7437743c2dc9158842b2f7d
a-wozniakowski <wozn0001@e.ntu.edu.sg>
2020-07-12 16:59:52 +0800

    Initial tutorial

---
+++ examples/basics/multi_target.py
@@ -0,0 +1,56 @@
+"""
+============================
+Multi-target regression
+============================
+
+This example introduces the Regressor object in a
+multi-target regression task. 
+"""
+
+import pandas as pd
+
+from sklearn.datasets import load_linnerud
+from sklearn.model_selection import train_test_split
+
+from physlearn import Regressor
+
+
+# Load the data from Sklearn
+bunch = load_linnerud(as_frame=True)  # returns a Bunch instance
+X, y = bunch['data'], bunch['target']
+
+# Split the data, using the default test_size=0.25.
+# X_train has shape (15, 3), y_train has shape (15, 3)
+# X_test has shape (5, 3), and y_test has shape (5, 3).
+# Namely, there are 3 features and 3 single-target regression subtasks.
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+
+# Choose the underlying regressor to be the Sklearn
+# histogram-based gradient boosting regressor.
+regressor_choice = 'HistGradientBoostingRegressor'
+
+# Choose the Sklearn QuantileTransformer as the data preprocessor.
+# The output distribution is the Gaussian, e.g., 'normal'.
+# The number of quantiles is the number of examples in y_train,
+# e.g., 15.
+pipeline_transform = 'quantilenormal'
+
+# Make an instance of the Regressor object.
+reg = Regressor(regressor_choice=regressor_choice, pipeline_transform=pipeline_transform)
+
+
+# Greedily fit the additive model
+reg.fit(X_train, y_train)
+
+# Generate test data predictions
+y_pred = reg.fit(X_train, y_train).predict(X_test)
+
+# Evaluate the test error, and store
+# the results as a DataFrame
+score = reg.score(y_test, y_pred)
+
+# Print the mean absolute error, mean squared error,
+# root mean squared error, R2, the expected variance,
+# and the mean squared log error for each individual
+# single-target regression subtask.
+print(score)
---
+++ examples/basics/pipeline_transform.py
@@ -0,0 +1,58 @@
+"""
+============================
+Custom pipeline transform
+============================
+
+This example extends the introduction, wherein we use
+a custom pipeline transform.
+"""
+
+import pandas as pd
+
+from sklearn.datasets import load_boston
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+from physlearn import Regressor
+
+
+# Load the data from Sklearn
+X, y = load_boston(return_X_y=True)
+X, y = pd.DataFrame(X), pd.Series(y)
+
+# Split the data, using the default test_size=0.25.
+# X_train has shape (379, 13), y_train has shape (379,)
+# X_test has shape (127, 13), and y_test has shape (127,).
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+
+# Choose the underlying regressor to be the LightGBM LGBMRegressor.
+# The regressor choice is a special parameter and it is case-insensitive.
+regressor_choice = 'lgbmregressor'
+
+# We create a list of tuples, wherein each tuple contains a name
+# and a transform. In the list below, the first element is a 
+# tuple with the name 'tr1' and the transform StandardScaler().
+# In general, the pipeline transform parameter can be a tuple,
+# as in ('tr', StandardScaler()), or it can be a list, as in
+# [('tr', StandardScaler()].
+pipeline_transform = [('tr1', StandardScaler()), ('tr2', StandardScaler())]
+
+# Make an instance of the Regressor object.
+reg = Regressor(regressor_choice=regressor_choice, pipeline_transform=pipeline_transform)
+
+
+# Greedily fit the additive model
+reg.fit(X_train, y_train)
+
+# Generate test data predictions
+y_pred = reg.fit(X_train, y_train).predict(X_test)
+
+# Evaluate the test error, and store
+# the results as a DataFrame
+score = reg.score(y_test, y_pred)
+
+# Print the mean absolute error, mean squared error,
+# root mean squared error, R2, the expected variance,
+# and the mean squared log error for the single-target
+# regression task.
+print(score)
